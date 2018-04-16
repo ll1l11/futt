c1bd2ee4308b0b7f18a884bdd577cdc6ea05e556
codeif <me@codeif.com>
2018-04-16 18:04:29 +0800

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='futt',
+    version='0.0.1',
+    packages=['futt'],
+    url='https://github.com/codeif/futt',
+    author='codeif',
+    author_email='me@codeif.com',
+    description='flask单元测试用的工具 Flask Unit Test Tools.'
+)
